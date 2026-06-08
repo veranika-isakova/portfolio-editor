35e57ebbe326af7eb2f7078cfe1b3eda83660332
Veranika Isakova <veranikaisakova@Veranika-Isakova.local>
2026-06-08 13:24:43 +0200

    order

--- app/projects/ny/page.tsx
+++ app/projects/ny/page.tsx
@@ -45,8 +45,8 @@ export default function DreamPage() {
       {/* FIRST ROW */}
 
       <div className="screenshots-grid">
-        <img src="/images/ny/face.jpg" alt="" />
-        <img src="/images/ny/alone.jpg" alt="" />
+        <img src="/images/ny/city.jpg" alt="" />
+        <img src="/images/ny/ring.jpg" alt="" />
       </div>
 
       {/* FULL WIDTH IMAGE */}
@@ -60,7 +60,7 @@ export default function DreamPage() {
       {/* SECOND ROW */}
 
       <div className="screenshots-grid">
-        <img src="/images/ny/city.jpg" alt="" />
+        <img src="/images/ny/alone.jpg" alt="" />
         <img src="/images/ny/ny.jpg" alt="" />
       </div>
 
