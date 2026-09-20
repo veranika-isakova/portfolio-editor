2920acaf5fbe1d72a1aa4f4b2a1ca1902fc9e6bf
Veranika Isakova <veranikaisakova@Veranika-Isakova.local>
2026-09-20 18:20:30 +0200

    remove temporary

--- app/page.tsx
+++ app/page.tsx
@@ -19,20 +19,6 @@ export default function Home() {
       />
 
       <ProjectCard
-        href="/projects/go"
-        image="/images/go/cover.jpg"
-        title="Letting Go"
-        subtitle="Short Film"
-      />
-
-      <ProjectCard
-        href="/projects/ny"
-        image="/images/ny/cover.jpg"
-        title="City drift"
-        subtitle="Cinematic portrait film"
-      />
-
-      <ProjectCard
         href="/projects/pavel"
         image="/images/pavel/cover.jpg"
         title="Pavel Fakhrtdinov"
@@ -54,6 +40,13 @@ export default function Home() {
       />
 
       <ProjectCard
+        href="/projects/ny"
+        image="/images/ny/cover.jpg"
+        title="City drift"
+        subtitle="Cinematic portrait film"
+      />
+
+      <ProjectCard
         href="/projects/dream"
         image="/images/dream/cover.jpg"
         title="In a dream maybe"
@@ -66,6 +59,13 @@ export default function Home() {
         title="Lime"
         subtitle="Studio campaign"
       />
+
+      {/* <ProjectCard
+        href="/projects/go"
+        image="/images/go/cover.jpg"
+        title="Letting Go"
+        subtitle="Short Film"
+      /> */}
 
     </main>
   )
